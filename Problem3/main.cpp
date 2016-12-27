f7dfa635954e2e5064e5f30d493d9d57f29f1f1d
snwfdhmp <snwfdhmp@protonmail.com>
2016-12-27 12:27:55 +0100

    Improve Solution

--- Problem3/main.cpp
+++ Problem3/main.cpp
@@ -6,10 +6,11 @@
 int main(int argc, char const *argv[])
 {
 	time_t t_start, t_end;
-	time(&t_start);
-
 	unsigned long long int n;
 	int i, b=1;
+
+	time(&t_start);
+
 	for(n=INC; (b!=0); n+=INC){
 		b = 0;
 		for(i=1;i<=20;i++){
@@ -18,10 +19,13 @@ int main(int argc, char const *argv[])
 		
 	}
 	n-=INC;
+
+	time(&t_end);
+
 	for(i=1;i<=20;i++){
 		b+=(n%i);
 	}
-	time(&t_end);
+	
 	printf("N founded : %llu (b=%d) (in %.0f seconds)\n", n, b, difftime(t_end, t_start));
 	return 0;
 }
