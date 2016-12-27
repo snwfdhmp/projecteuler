36a2dbf44b6bc37e5aba46f9397257afde6e5c1c
snwfdhmp <snwfdhmp@protonmail.com>
2016-12-27 12:26:59 +0100

    Improve Solution

--- Problem3/README.md
+++ Problem3/README.md
@@ -1,3 +1,4 @@
 First test : found in 43 seconds
 Second test : found in 33 seconds (by reducing printf frequency)
-Third test : found 232792562 in 16 seconds (by testing only n with %2 == 0)
+Third test : found 232792560 in 16 seconds (by testing only n with %2 == 0)
+Fourth test : found 232792560 in 3 seconds (with n%20 == 0 and reducing printf freq)
--- Problem3/main.cpp
+++ Problem3/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 
+#define INC 20
+
 int main(int argc, char const *argv[])
 {
 	time_t t_start, t_end;
@@ -8,19 +10,16 @@ int main(int argc, char const *argv[])
 
 	unsigned long long int n;
 	int i, b=1;
-	for(n=20; (b!=0); n+=20){
-		b = n%20;
-		if(n%10000==0)
-			printf("\r%llu", n);
+	for(n=INC; (b!=0); n+=INC){
+		b = 0;
 		for(i=1;i<=20;i++){
-			b+=(n%i);
+			b+=(n%i);	
 		}
+		
 	}
-	printf("Result : \n");
-	b = 0;
-	for(i=1; i<=20; i++) {
-		b+=n%i;
-		printf("%llu mod %d = %d (b=%d)\n", n, i, n%i, b);
+	n-=INC;
+	for(i=1;i<=20;i++){
+		b+=(n%i);
 	}
 	time(&t_end);
 	printf("\nN founded : %llu (b=%d) (in %.0f seconds)\n", n, b, difftime(t_end, t_start));
